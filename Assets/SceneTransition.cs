2371ef5f26320fcc04262a1c35779879f85e04af
Pedro Cuadra <pedro.cuadra@alumnos.ucn.cl>
2022-05-26 09:01:28 -0400

    ayudantía 6

---
+++ Assets/SceneTransition.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTransition : MonoBehaviour
+{
+    public void ChangeScene(string name){
+        
+    }
+}
