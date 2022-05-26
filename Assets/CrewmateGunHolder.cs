daf8f82ce91fede3610fb9e281ac1f32dd606ab4
Pedro Cuadra <pedro.cuadra@alumnos.ucn.cl>
2022-05-25 23:30:39 -0400

    Bugfix: la pistola no se tomaba en su lugar

--- Assets/CrewmateGunHolder.cs
+++ Assets/CrewmateGunHolder.cs
@@ -5,22 +5,45 @@ using UnityEngine;
 public class CrewmateGunHolder : MonoBehaviour
 {
     public Transform gunSpawnPoint;
+    public GameObject handPrefab;
+    public Transform dropTransform;
     private Gun gun;
 
+    void Awake(){
+        handPrefab.SetActive(false);
+    }
+
     void Update(){
-        if(Input.GetKeyDown(KeyCode.E) && gun != null){
+        if(gun == null)
+            return;
+
+        if(Input.GetKeyDown(KeyCode.E))
             gun.Shoot();
-        }
+
+        if(Input.GetKeyDown(KeyCode.R))
+            DropGun();
     }
 
     void OnCollisionEnter2D(Collision2D collision){
-        Gun g = collision.gameObject.GetComponent<Gun>();
+        GameObject go  = collision.gameObject;
+        Gun g = go.GetComponent<Gun>();
         if(g==null)
             return;
         
-        collision.gameObject.transform.parent = gunSpawnPoint;
-        collision.gameObject.transform.position = Vector3.zero;
-        collision.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        go.transform.parent = gunSpawnPoint;
+        go.transform.position = gunSpawnPoint.position;
+        go.transform.rotation = gunSpawnPoint.rotation;
+        go.transform.localScale = gunSpawnPoint.localScale;
+        go.GetComponent<BoxCollider2D>().enabled = false;
         gun = g;
+        handPrefab.SetActive(true);
+    }
+
+    void DropGun(){
+        gun.transform.localPosition = dropTransform.localPosition;
+        gun.transform.parent = null;
+        gun.GetComponent<BoxCollider2D>().enabled = true;
+        gun = null;
+        handPrefab.SetActive(false);
     }
 }
