9fa20f8a239ad0cbd8b90add8d0540b95ddbe7bf
Pedro Cuadra <pedro.cuadra@alumnos.ucn.cl>
2022-06-16 09:03:57 -0400

    cambios ayudantia presencial

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,9 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
